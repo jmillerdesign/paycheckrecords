f215ba5f12b587f432102d14d03361275d67089a
J. Miller <j@jmillerdesign.com>
2014-12-16 00:45:38 -0800

    Use prompt and datejs to ask for hours

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-var username    = '',
-    password    = '',
-    shift1Start = '8a',
-    shift1End   = '12p',
-    shift2Start = '1p',
-    shift2End   = '5p',
-    // Default to today, or you can manually set a date
-    date        = (new Date().getMonth() + 1) + '/' + new Date().getDate() + '/' + new Date().getFullYear(),
-    // date        = '10//2014',
-    Nightmare   = require('nightmare');
+require('datejs');
+var config    = require('./config');
+var prompt    = require('prompt');
+var Nightmare = require('nightmare');
 
-if (!username) console.error('Missing username');
-if (!password) console.error('Missing password');
-if (!date)     console.error('Missing date');
+var schema = {
+	properties: {
+		date: {
+			description: 'Date',
+			default: 'today'
+		},
+		shift1: {
+			description: 'Before lunch',
+			default: '8a-11a'
+		},
+		shift2: {
+			description: 'After lunch',
+			default: '12p-5p'
+		}
+	}
+};
 
-new Nightmare()
-	.goto('https://www.paycheckrecords.com/login.jsp')
-	.type('#userStrId', username)
-	.type('#password', password)
-	.click('#Login')
-	.wait()
-	.type('#date', date)
-	.type('#startTime', shift1Start)
-	.type('#endTime', shift1End)
-	.click('#addTimeCardLine')
-	.wait()
-	.type('#date', date)
-	.type('#startTime', shift2Start)
-	.type('#endTime', shift2End)
-	.click('#addTimeCardLine')
-	.wait()
-	.run(function (err, nightmare) {
-		if (err) return console.error(err);
-		console.log('Done adding hours for ' + date);
-	});
+prompt.start();
+prompt.get(schema, function (err, result) {
+	if (err) throw err;
+
+	var date = Date.parse(result.date),
+	    date = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear(),
+	    shift1Start = result.shift1.split('-')[0].trim(),
+	    shift1End   = result.shift1.split('-')[1].trim()
+	    shift2Start = result.shift2.split('-')[0].trim(),
+	    shift2End   = result.shift2.split('-')[1].trim()
+
+	new Nightmare()
+		.goto('https://www.paycheckrecords.com/login.jsp')
+		.type('#userStrId', config.username)
+		.type('#password', config.password)
+		.click('#Login')
+		.wait()
+		.type('#date', date)
+		.type('#startTime', shift1Start)
+		.type('#endTime', shift1End)
+		.click('#addTimeCardLine')
+		.wait()
+		.type('#date', date)
+		.type('#startTime', shift2Start)
+		.type('#endTime', shift2End)
+		.click('#addTimeCardLine')
+		.wait()
+		.run(function (err, nightmare) {
+			if (err) throw err;
+
+			console.log('Done adding hours for ' + date);
+		});
+});
