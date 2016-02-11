faa48624de7db2ae308e4f8fb8a5f9b3d5cf3acb
J. Miller <j@jmillerdesign.com>
2016-02-10 20:08:30 -0800

    Update and improve script
    Use electron instead of phantom
    Use dialog instead of notification
    Don't use redis

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
 require('datejs');
-var config    = require('./config');
-var prompt    = require('prompt');
-var Nightmare = require('nightmare');
-var colors    = require('colors');
-var notifier  = require('node-notifier');
-var CronJob   = require('cron').CronJob;
-var Redis     = require('redis').createClient(config.redis.port, config.redis.host);
+const config    = require('./config');
+const chalk     = require('chalk');
+const CronJob   = require('cron').CronJob;
+const dialog    = require('dialog');
+const Nightmare = require('nightmare');
+const prompt    = require('prompt');
 
-// Monkey patch for https://github.com/segmentio/nightmare/issues/126
-(function () {
-	var sockjs = require('nightmare/node_modules/phantom/node_modules/shoe/node_modules/sockjs');
-	if (!sockjs._createServerOld) {
-		sockjs._createServerOld = sockjs.createServer;
-		sockjs.createServer = function (options) {
-			if (!options) {
-				options = {};
-			}
-			if (!('heartbeat_delay' in options)) {
-				options.heartbeat_delay = 200;
-			}
-			return sockjs._createServerOld(options);
-		};
-	}
-})();
-
-function enterHours (data) {
-	var date = Date.parse(data.date),
+function enterHours(params) {
+	var date = Date.parse(params.date),
 	    date = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear(),
-	    shift1Start = data.shift1.split('-')[0].trim(),
-	    shift1End   = data.shift1.split('-')[1].trim(),
-	    shift2Start = data.shift2.split('-')[0].trim(),
-	    shift2End   = data.shift2.split('-')[1].trim();
+	    shift1Start = params.shift1.split('-')[0].trim(),
+	    shift1End   = params.shift1.split('-')[1].trim(),
+	    shift2Start = params.shift2.split('-')[0].trim(),
+	    shift2End   = params.shift2.split('-')[1].trim();
 
 	// If you don't enter a time, then skip the entry.
 	// This way, you can enter "-" or something to skip
@@ -41,29 +23,31 @@ function enterHours (data) {
 		shift2Start = shift2End = '';
 	}
 
-	new Nightmare()
+	new Nightmare({show: false})
 		.goto('https://www.paycheckrecords.com/login.jsp')
-		.type('#userStrId', config.username)
-		.type('#password', config.password)
-		.click('#Login')
-		.wait()
-		.type('#date', date)
-		.type('#startTime', shift1Start)
-		.type('#endTime', shift1End)
+		.insert('#ius-userid', config.username)
+		.insert('#ius-password', config.password)
+		.click('#ius-sign-in-submit-btn')
+		.wait('#date')
+		.insert('#date', date)
+		.insert('#startTime', shift1Start)
+		.insert('#endTime', shift1End)
 		.click('#addTimeCardLine')
-		.wait()
-		.type('#date', date)
-		.type('#startTime', shift2Start)
-		.type('#endTime', shift2End)
+		.wait(1000) // Wait for ajax to submit
+		.wait('#date')
+		.insert('#date', date)
+		.type('#startTime') // Handle error when first shift already existed
+		.insert('#startTime', shift2Start)
+		.type('#endTime')   // Handle error when first shift already existed
+		.insert('#endTime', shift2End)
 		.click('#addTimeCardLine')
-		.wait()
-		.run(function (err, nightmare) {
+		.run(function (err) {
 			if (err) throw err;
 
-			console.log(('Done adding hours for ' + date).green);
-		});
+			console.log(chalk.green('Done adding hours for ' + date));
+		})
+		.end();
 }
-
 
 if (process.argv[0] === 'node') {
 	// Run manually
@@ -83,34 +67,20 @@ if (process.argv[0] === 'node') {
 			}
 		}
 	};
-	prompt.start();
-	prompt.get(schema, function (err, result) {
+	prompt.start().get(schema, function (err, result) {
 		if (err) throw err;
+
 		enterHours(result);
 	});
 } else {
 	// Run via forever
-	var job = new CronJob(config.cron, function () {
-		var date = Date.parse(config.defaults.date),
-		    date = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear(),
-		    hash = 'paycheckrecords_' + date;
+	new CronJob(config.cron, function () {
+		dialog.info('Did you work normal hours today?', 'Paycheck Records', ['No', 'Yes'], function (err, result) {
+			if (err) throw err;
 
-		Redis.get(hash, function (err, reply) {
-			if (reply) return; // Hours already entered for today
-			notifier.notify({
-				title: 'Punch Clock',
-				message: 'Are you working today?',
-				sound: 'Ping',
-				wait: true
-			});
-			notifier.on('click', function (notifierObject, options) {
-				var expire = 60 * 60 * 24 * 7 * 2; // Two weeks
-				Redis.setex(hash, expire, 'true');
-				var data = config.defaults;
-				enterHours(data);
-			});
+			if (result.trim() === 'button returned:Yes') {
+				enterHours(config.defaults);
+			}
 		});
-	});
-	job.start();
-	console.log('Cron started');
+	}).start();
 }
